7d084577429d907ba7a7898222312e7e12b48aaf
RishankD <156447323+RishankD@users.noreply.github.com>
2024-11-09 21:47:09 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// This define prices for each ingredient
+const ingredientPrices = {
+    Banana: 0.5,
+    Strawberry: 0.75,
+    Blueberry: 0.85,
+    Mango: 1.0,
+    Spinach: 0.6,
+    Kale: 0.7,
+    AlmondMilk: 1.0,
+    CoconutWater: 0.8,
+    Honey: 0.3,
+    ProteinPowder: 1.5
+};
